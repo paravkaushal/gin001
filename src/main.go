cbdd92e0b737c790a93700bb0e8a12153e151824
Parav Kaushal <parav.kaushal@velotio.com>
2022-02-22 11:06:56 +0530

    accept post request

--- src/main.go
+++ src/main.go
@@ -12,9 +12,15 @@ func HomePage(c *gin.Context) {
 	})
 }
 
+func PostHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Post Home Page",
+	})
+}
 func main() {
 	fmt.Println("Hello World!")
 	r := gin.Default()
 	r.GET("/", HomePage)
+	r.POST("/", PostHomePage)
 	r.Run()
 }
