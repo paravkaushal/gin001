9b9f5b38d8556b8f96ce2c122cac6da26e7c1005
Parav Kaushal <parav.kaushal@velotio.com>
2022-02-22 10:06:08 +0530

    server accepts get request

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World",
+	})
+}
+
+func main() {
+	fmt.Println("Hello World!")
+	r := gin.Default()
+	r.GET("/", HomePage)
+	r.Run()
+}
